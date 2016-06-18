a0d6c5b29669efd8d15d6600cb49dfe8e346fc49
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-06-17 20:09:16 -0500

    adding more vcf search

---
+++ vcfsearch.h
@@ -0,0 +1,13 @@
+//
+// Created by jameswhang on 6/17/16.
+//
+
+#ifndef VCF_SIM_VCFSEARCH_H
+#define VCF_SIM_VCFSEARCH_H
+
+class vcfsearch{
+public:
+    int match_positions();
+};
+
+#endif //VCF_SIM_VCFSEARCH_H
