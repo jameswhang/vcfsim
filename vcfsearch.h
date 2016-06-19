0e450fdb455a3d6725d6e2eb58b0403042c16174
James Whang <sungyoonwhang2017@u.northwestern.edu>
2016-06-18 23:16:58 -0500

    more headers

--- vcfsearch.h
+++ vcfsearch.h
@@ -1,13 +1,17 @@
 //
 // Created by jameswhang on 6/17/16.
 //
+#include <map>
+#include <vector>
 
 #ifndef VCF_SIM_VCFSEARCH_H
 #define VCF_SIM_VCFSEARCH_H
 
 class vcfsearch{
+map<string, vector<int>> info;
 public:
     int match_positions();
+    int ();
 };
 
 #endif //VCF_SIM_VCFSEARCH_H
